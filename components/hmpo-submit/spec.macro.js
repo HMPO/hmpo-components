1cab1ff3682d89b496b33da88706162c2fad9d8c
Paul Winkler <paul.winkler@digital.homeoffice.gov.uk>
2019-05-02 11:42:19 +0100

    Add id to button attributes

--- components/hmpo-submit/spec.macro.js
+++ components/hmpo-submit/spec.macro.js
@@ -7,9 +7,16 @@ describe('hmpoSubmit', () => {
         locals = {};
     });
 
-    it('renders with id', () => {
+    it('renders with localisation text', () => {
         const $ = render('hmpoSubmit', { key: 'myButtonTextKey' }, locals);
         const $component = $('.govuk-button');
         expect($component.text()).to.contain('buttons.myButtonTextKey');
+    });
+
+    it('renders with id', () => {
+        const $ = render('hmpoSubmit', { id: 'myid' }, locals);
+        const $component = $('.govuk-button');
+        expect($component.attr('id')).to.equal('myid');
+        expect($component.attr('name')).to.equal('myid');
     });
 });
