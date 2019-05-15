ff59693bd7d736b9aa6dbb034a4e1fa9db320340
Paul Winkler <paul.winkler@digital.homeoffice.gov.uk>
2019-05-15 16:37:04 +0100

    Add callable warning text component

--- components/hmpo-close/spec.macro.js
+++ components/hmpo-close/spec.macro.js
@@ -3,19 +3,19 @@
 describe('hmpoClose', () => {
     it('renders with text', () => {
         const $ = render({ component: 'hmpoClose', params: { text: 'test' } });
-        const $component = $('button');
-        expect(cleanHtml($component)).to.equal('test');
+        const $component = $('div');
+        expect($component.attr('data-button')).to.equal('  <button type="button" class="govuk-button">  test</button>');
     });
 
     it('renders with html', () => {
         const $ = render({ component: 'hmpoClose', params: { html: '<b>html</b>' } });
-        const $component = $('button');
-        expect(cleanHtml($component)).to.equal('<b>html</b>');
+        const $component = $('div');
+        expect($component.attr('data-button')).to.equal('  <button type="button" class="govuk-button">  <b>html</b></button>');
     });
 
     it('renders with default text', () => {
         const $ = render({ component: 'hmpoClose' });
-        const $component = $('button');
-        expect(cleanHtml($component)).to.equal('Close');
+        const $component = $('div');
+        expect($component.attr('data-button')).to.equal('  <button type="button" class="govuk-button">  Close</button>');
     });
 });
