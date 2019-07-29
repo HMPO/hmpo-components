8028f4171fd199a1353a19c676d1261d0cf7c72d
Paul Winkler <paul.winkler@digital.homeoffice.gov.uk>
2019-07-29 17:38:52 +0100

    Update components to govuk-frontend v3

--- components/hmpo-close/spec.macro.js
+++ components/hmpo-close/spec.macro.js
@@ -4,18 +4,18 @@ describe('hmpoClose', () => {
     it('renders with text', () => {
         const $ = render({ component: 'hmpoClose', params: { text: 'test' } });
         const $component = $('div');
-        expect($component.attr('data-button')).to.equal('  <button type="button" class="govuk-button">  test</button>');
+        expect($component.attr('data-button')).to.equal('  <button type="button" class="govuk-button" data-module="govuk-button">  test</button>');
     });
 
     it('renders with html', () => {
         const $ = render({ component: 'hmpoClose', params: { html: '<b>html</b>' } });
         const $component = $('div');
-        expect($component.attr('data-button')).to.equal('  <button type="button" class="govuk-button">  <b>html</b></button>');
+        expect($component.attr('data-button')).to.equal('  <button type="button" class="govuk-button" data-module="govuk-button">  <b>html</b></button>');
     });
 
     it('renders with default text', () => {
         const $ = render({ component: 'hmpoClose' });
         const $component = $('div');
-        expect($component.attr('data-button')).to.equal('  <button type="button" class="govuk-button">  Close</button>');
+        expect($component.attr('data-button')).to.equal('  <button type="button" class="govuk-button" data-module="govuk-button">  Close</button>');
     });
 });
