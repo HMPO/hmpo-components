5d8575bc5182d04dd8faa11e11306ae6f56f629f
Paul Winkler <paul.winkler@digital.homeoffice.gov.uk>
2022-11-19 01:41:26 +0000

    Add ability to render html reveals for radio buttons by speciying only reveal localisation for an item

--- components/hmpo-radios/spec.macro.js
+++ components/hmpo-radios/spec.macro.js
@@ -10,6 +10,9 @@ describe('hmpoRadios', () => {
                     'my-input': {
                         validate: 'required',
                         items: ['a', 'b', 'c']
+                    },
+                    'my-input-reveals': {
+                        items: ['a', 'b', 'c']
                     }
                 }
             },
@@ -20,22 +23,22 @@ describe('hmpoRadios', () => {
     });
 
     it('renders with id', () => {
-        const $ = render({component: 'hmpoRadios', params: {id: 'my-input'}, ctx: true}, locals);
+        const $ = render.withLocale({component: 'hmpoRadios', params: {id: 'my-input'}, ctx: true}, locals);
         const $component = $('.govuk-fieldset');
         expect($component.attr('id')).to.equal('my-input-fieldset');
     });
 
     it('renders with legend and hint', () => {
-        const $ = render({component: 'hmpoRadios', params: {id: 'my-input'}, ctx: true}, locals);
+        const $ = render.withLocale({component: 'hmpoRadios', params: {id: 'my-input'}, ctx: true}, locals);
 
         const $legend = $('.govuk-fieldset__legend');
-        expect($legend.text().trim()).to.equal('[fields.my-input.legend]');
+        expect($legend.text().trim()).to.equal('fields.my-input.label');
         const $hint = $('.govuk-hint');
-        expect($hint.text().trim()).to.equal('[fields.my-input.hint]');
+        expect($hint.text().trim()).to.equal('Hint text');
     });
 
     it('renders items with ids and labels', () => {
-        const $ = render({component: 'hmpoRadios', params: {id: 'my-input'}, ctx: true}, locals);
+        const $ = render.withLocale({component: 'hmpoRadios', params: {id: 'my-input'}, ctx: true}, locals);
 
         const $item1 = $('.govuk-radios__input').eq(0);
         expect($item1.attr('name')).to.equal('my-input');
@@ -43,7 +46,7 @@ describe('hmpoRadios', () => {
         expect($item1.attr('id')).to.equal('my-input');
         expect($item1.attr('checked')).to.be.undefined;
         const $itemlabel1 = $('.govuk-radios__label').eq(0);
-        expect($itemlabel1.text().trim()).to.equal('[fields.my-input.items.a.label]');
+        expect($itemlabel1.text().trim()).to.equal('fields.my-input.items.a.label');
         expect($itemlabel1.attr('id')).to.equal('my-input-a-label');
 
         const $item2 = $('.govuk-radios__input').eq(1);
@@ -52,7 +55,7 @@ describe('hmpoRadios', () => {
         expect($item2.attr('id')).to.equal('my-input-b');
         expect($item2.attr('checked')).to.equal('checked');
         const $itemlabel2 = $('.govuk-radios__label').eq(1);
-        expect($itemlabel2.text().trim()).to.equal('[fields.my-input.items.b.label]');
+        expect($itemlabel2.text().trim()).to.equal('fields.my-input.items.b.label');
         expect($itemlabel2.attr('id')).to.equal('my-input-b-label');
     });
 
@@ -66,7 +69,7 @@ describe('hmpoRadios', () => {
             '1': { text: 'one' },
             'a': { text: 'alpha' }
         };
-        const $ = render({component: 'hmpoRadios', params: {id: 'my-input', items: paramItems}, ctx: true}, locals);
+        const $ = render.withLocale({component: 'hmpoRadios', params: {id: 'my-input', items: paramItems}, ctx: true}, locals);
 
         const $item1 = $('.govuk-radios__input').eq(0);
         expect($item1.attr('value')).to.equal('1');
@@ -113,21 +116,21 @@ describe('hmpoRadios', () => {
     });
 
     it('renders default items', () => {
-        const $ = render({component: 'hmpoRadios', params: {id: 'default-input'}, ctx: true}, locals);
+        const $ = render.withLocale({component: 'hmpoRadios', params: {id: 'default-input'}, ctx: true}, locals);
 
         const $item1 = $('.govuk-radios__input').eq(0);
         expect($item1.attr('value')).to.equal('true');
         const $itemlabel1 = $('.govuk-radios__label').eq(0);
-        expect($itemlabel1.text().trim()).to.equal('[fields.default-input.items.true.label]');
+        expect($itemlabel1.text().trim()).to.equal('fields.default-input.items.true.label');
 
         const $item2 = $('.govuk-radios__input').eq(1);
         expect($item2.attr('value')).to.equal('false');
         const $itemlabel2 = $('.govuk-radios__label').eq(1);
-        expect($itemlabel2.text().trim()).to.equal('[fields.default-input.items.false.label]');
+        expect($itemlabel2.text().trim()).to.equal('fields.default-input.items.false.label');
     });
 
     it('renders items with conditionals', () => {
-        const $ = render({
+        const $ = render.withLocale({
             component: 'hmpoRadios', ctx: true, params: {
                 id: 'my-input', conditionals: {
                     a: {id: 'a', html: 'a <b>string</b>'},
@@ -143,7 +146,7 @@ describe('hmpoRadios', () => {
     });
 
     it('renders items with conditionals inline', () => {
-        const $ = render({
+        const $ = render.withLocale({
             component: 'hmpoRadios', ctx: true, params: {
                 id: 'my-input', inline: true, conditionals: {
                     a: {id: 'a', html: 'a <b>first</b>'},
@@ -166,8 +169,23 @@ describe('hmpoRadios', () => {
         expect(cleanHtml($item2)).to.equal('b <b>second</b>');
     });
 
+    it('renders items with conditionals from localisation', () => {
+        const $ = render.withLocale({
+            component: 'hmpoRadios', ctx: true, params: {
+                id: 'my-input-reveals', conditionals: {
+                    a: {id: 'a', html: 'a <b>first</b>'}
+                }
+            }
+        }, locals);
+
+        const $item1 = $('.govuk-radios__conditional').eq(0);
+        expect(cleanHtml($item1)).to.equal('a <b>first</b>');
+        const $item2 = $('.govuk-radios__conditional').eq(1);
+        expect(cleanHtml($item2)).to.equal('<p>second</p>');
+    });
+
     it('renders items with inline conditionals with no html', () => {
-        const $ = render({
+        const $ = render.withLocale({
             component: 'hmpoRadios', ctx: true, params: {
                 id: 'my-input', inline: true,  conditionals: {
                     a: {id: 'a', html: 'a <b>string</b>'},
@@ -180,7 +198,7 @@ describe('hmpoRadios', () => {
     });
 
     it('renders items with inline conditionals with multiConditional flag', () => {
-        const $ = render({
+        const $ = render.withLocale({
             component: 'hmpoRadios', ctx: true, params: {
                 id: 'my-input', inline: true,  conditionals: {
                     a: {id: 'a', html: 'a <b>string</b>'},
@@ -194,7 +212,7 @@ describe('hmpoRadios', () => {
     });
 
     it('renders items with conditionals without inset', () => {
-        const $ = render({
+        const $ = render.withLocale({
             component: 'hmpoRadios', ctx: true, params: {
                 id: 'my-input', inline: true, conditionals: {
                     a: {id: 'a', html: 'a <b>first</b>', removeInset: true},
@@ -212,17 +230,17 @@ describe('hmpoRadios', () => {
     });
 
     it('renders radio buttons with header', () => {
-        const $ = render({component: 'hmpoRadios', params: {id: 'my-input', isPageHeading: true}, ctx: true}, locals);
+        const $ = render.withLocale({component: 'hmpoRadios', params: {id: 'my-input', isPageHeading: true}, ctx: true}, locals);
         const $legend = $('.govuk-fieldset__legend');
         expect($legend.attr('class')).to.equal('govuk-fieldset__legend govuk-fieldset__legend--l');
-        expect(cleanHtml($legend)).to.equal('<h1 class="govuk-fieldset__heading">[fields.my-input.legend]</h1>');
+        expect(cleanHtml($legend)).to.equal('<h1 class="govuk-fieldset__heading">fields.my-input.label</h1>');
     });
 
     it('renders radio buttons with header attributes', () => {
-        const $ = render({component: 'hmpoRadios', params: {id: 'my-input', isPageHeading: true, legend: { attributes: { 'data-test': 'test value' }}}, ctx: true}, locals);
+        const $ = render.withLocale({component: 'hmpoRadios', params: {id: 'legendtest', isPageHeading: true, legend: { attributes: { 'data-test': 'test value' }}}, ctx: true}, locals);
         const $legend = $('.govuk-fieldset__legend');
         expect($legend.attr('class')).to.equal('govuk-fieldset__legend govuk-fieldset__legend--l');
-        expect(cleanHtml($legend)).to.equal('<h1 class="govuk-fieldset__heading"><span data-test="test value">[fields.my-input.legend]</span></h1>');
+        expect(cleanHtml($legend)).to.equal('<h1 class="govuk-fieldset__heading"><span data-test="test value">Legend text</span></h1>');
     });
 
     it('renders radio buttons with header label localisation instead of legend when legend is not present', () => {
