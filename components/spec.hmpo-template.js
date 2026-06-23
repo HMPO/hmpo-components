83eac321fcd9ba5caf7f056dbd7c30fee378dd9d
Imran Ali <imran.ali@digital.homeoffice.gov.uk>
2026-06-23 17:36:05 +0100

    DD-1368 Upgrade to Gov UK Frontend v6.5.0

--- components/spec.hmpo-template.js
+++ components/spec.hmpo-template.js
@@ -6,4 +6,17 @@ describe('hmpo template', () => {
             render({ template: 'hmpo-template.njk' });
         }).to.not.throw();
     });
+
+    it('renders GOV.UK Frontend v6 template blocks', () => {
+        const $ = render(
+            { template: 'hmpo-template.njk' },
+            { backLink: '/previous-page' }
+        );
+
+        expect($('.govuk-skip-link').length).to.equal(1);
+        expect($('.hmpo-cookie-banner').length).to.equal(1);
+        expect($('.govuk-header').length).to.equal(1);
+        expect($('.govuk-service-navigation').length).to.equal(1);
+        expect($('#back .govuk-back-link').attr('href')).to.equal('/previous-page');
+    });
 });
